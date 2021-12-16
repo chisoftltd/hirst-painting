4c39409196968acc7f52464affc015193db9bda2
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-16 00:34:44 +0000

    Complete work

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import colorgram
+import turtle as turtle_hirst
+import random
+from turtle import Screen
 
-rgb_colors = []
-# Extract 6 colors from an image.
-colors = colorgram.extract('image.jpg', 6)
-print(colors)
+screen = Screen()
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-
-print(rgb_colors)
+turtle_hirst.colormode(255)
+crazy = turtle_hirst.Turtle()
 color_list = [(240, 231, 221), (228, 152, 86), (120, 171, 203), (1, 0, 0), (40, 111, 159), (240, 201, 83)]
+for j in range(10):
+    crazy.penup()
+    crazy.goto(-100, 40 * j)
+    for _ in range(10):
+        crazy.penup()
+        crazy.dot(20, random.choice(color_list))
+        crazy.forward(35)
+
+
+screen.exitonclick()
