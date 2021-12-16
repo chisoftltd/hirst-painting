ef9f18682dd62ae62bc816640c0ce5e2a8d8732d
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-16 00:52:43 +0000

    Complete work

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import turtle as turtle_hirst
 import random
-from turtle import Screen
 
-screen = Screen()
+crazy = turtle_hirst.Turtle()
+screen = turtle_hirst.Screen()
+turtle_hirst.speed("fastest")
+crazy.hideturtle()
+crazy.penup()
+
 
 turtle_hirst.colormode(255)
-crazy = turtle_hirst.Turtle()
+
 color_list = [(240, 231, 221), (228, 152, 86), (120, 171, 203), (1, 0, 0), (40, 111, 159), (240, 201, 83)]
 for j in range(10):
-    crazy.penup()
-    crazy.goto(-100, 40 * j)
+    crazy.goto(-200, ((40 * j) - 200))
     for _ in range(10):
-        crazy.penup()
         crazy.dot(20, random.choice(color_list))
-        crazy.forward(35)
+        crazy.forward(50)
 
 
 screen.exitonclick()
